6eba67847af45222b8ef5602307ff146e745287b
Alex Watt <alex@alexcwatt.com>
2021-12-02 06:40:30 -0500

    Day 1, Part 2: Add window_size argument

--- 2021/01/sonar_sweep.rb
+++ 2021/01/sonar_sweep.rb
@@ -1,9 +1,9 @@
 class SonarSweep
   class << self
-    def call(depth_measurements:)
+    def call(depth_measurements:, window_size:)
       counter = 0
-      depth_measurements.each_cons(2) do |prev, curr|
-        counter += 1 if curr > prev
+      depth_measurements.each_cons(window_size).each_cons(2) do |prev, curr|
+        counter += 1 if curr.sum > prev.sum
       end
       counter
     end
--- 2021/01/sonar_sweep_test.rb
+++ 2021/01/sonar_sweep_test.rb
@@ -4,12 +4,18 @@ require_relative "sonar_sweep"
 
 class SonarSweepTest < MiniTest::Unit::TestCase
   def test_single_measurement
-    assert_equal 0, SonarSweep.call(depth_measurements: [199])
+    assert_equal 0, SonarSweep.call(depth_measurements: [199], window_size: 1)
   end
 
-  def test_official_case
+  def test_official_case_part_one
     measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 
-    assert_equal 7, SonarSweep.call(depth_measurements: measurements)
+    assert_equal 7, SonarSweep.call(depth_measurements: measurements, window_size: 1)
+  end
+
+  def test_official_case_part_two
+    measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+
+    assert_equal 5, SonarSweep.call(depth_measurements: measurements, window_size: 3)
   end
 end
